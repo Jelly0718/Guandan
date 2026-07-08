127745d7bd2feef09883c69255fef783676250eb
Jelly0718 <securejelly0718@gmail.com>
2026-07-08 16:19:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,85 @@
-const cacheName = "guandan-v2";
+const cacheName = "guandan-v3";
+
 
 const files = [
-    "./index.html",
-    "./manifest.json",
-    "./sw.js"
+
+"./index.html",
+"./manifest.json",
+"./sw.js"
+
 ];
 
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => cache.addAll(files))
-    );
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(cacheName)
+
+.then(cache=>
+
+cache.addAll(files)
+
+)
+
+);
+
 });
 
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                .filter(key => key !== cacheName)
-                .map(key => caches.delete(key))
-            );
-        })
-    );
+
+
+self.addEventListener(
+"activate",
+event=>{
+
+
+event.waitUntil(
+
+caches.keys()
+
+.then(keys=>{
+
+return Promise.all(
+
+keys
+
+.filter(k=>k!==cacheName)
+
+.map(k=>caches.delete(k))
+
+);
+
+})
+
+);
+
+
 });
 
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-    );
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+
 });
