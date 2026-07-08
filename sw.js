6d6ef17baae61e209a6dd42c51efdf2cb59b858a
Jelly0718 <securejelly0718@gmail.com>
2026-07-08 15:47:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const cache = "guandan-v1";
 
+const files = [
+"index.html",
+"manifest.json",
+"sw.js"
+];
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+caches.open(cache)
+.then(
+cache => cache.addAll(files)
+)
+);
+
+});
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(
+response =>
+response || fetch(event.request)
+)
+
+);
+
+});
