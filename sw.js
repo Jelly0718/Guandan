f07e979cd6c19b76975127f142ab67c5abf6b8ed
Jelly0718 <securejelly0718@gmail.com>
2026-07-08 16:06:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
-const cache = "guandan-v1";
+const cacheName = "guandan-v2";
 
 const files = [
-"index.html",
-"manifest.json",
-"sw.js"
+    "./index.html",
+    "./manifest.json",
+    "./sw.js"
 ];
 
 
-self.addEventListener(
-"install",
-event => {
-
-event.waitUntil(
-caches.open(cache)
-.then(
-cache => cache.addAll(files)
-)
-);
-
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(files))
+    );
 });
 
 
-self.addEventListener(
-"fetch",
-event => {
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                .filter(key => key !== cacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
 
-event.respondWith(
 
-caches.match(event.request)
-.then(
-response =>
-response || fetch(event.request)
-)
-
-);
-
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
